87c91c1e068ff2d1058d34f3bbbaf5cc68cc471d
Spaida Nsabwe <spaidaliphava@gmail.com>
2025-10-25 06:52:04 +0200

    Delete User.js

--- User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  online: { type: Boolean, default: false },
-});
-
-export default mongoose.model("User", userSchema);
